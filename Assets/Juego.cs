5cf4bc27323486b4fffa222a460b88ce66434bf8
Kazui97 <camyescobar12@gmail.com>
2019-10-09 12:50:23 -0500

    inicio
    :dragon:

---
+++ Assets/Juego.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juego : MonoBehaviour
+{
+    public int varx;
+    public int vary;
+    public GameObject buey;
+    public GameObject palomo;
+    public GameObject mono;
+    public GameObject dragon;
+    public GameObject [,]gameObjects;
+
+
+    void Start()
+    {
+        /*gameObjects = new GameObject[varx , vary];
+        for (int x = 0; x < varx; x++)
+        {
+            for (int y = 0; y < vary; y++)
+            {
+                GameObject buuey = GameObject.Instantiate(buey) as GameObject;
+                Vector3 position = new Vector3(x,y,0);
+                buuey.transform.position = position;
+                gameObjects[x,y]=buuey;
+              
+            }
+        }*/
+
+        int cambio = Random.Range(0,5)
+        switch (switch_on)
+        {
+            
+            default:
+        }
+
+
+
+
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Rotar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotar : MonoBehaviour
+{
+    public bool rotar = false;
+
+    public void OnMouseDown()
+    {
+        rotar = true;
+        Invoke( "rotara", 5);
+    }
+        void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (rotar)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.up *180, Time.deltaTime));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, Vector3.zero, Time.deltaTime));
+        }
+    }
+    public void rotara()
+    {
+        rotar = false;
+    }
+}
