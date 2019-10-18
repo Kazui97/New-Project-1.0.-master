0e1ca1b281f9e4e763d0a7179fe5a909267f4ff7
Kazui97 <camyescobar12@gmail.com>
2019-10-18 00:34:44 -0500

    ay

--- Assets/Juego.cs
+++ Assets/Juego.cs
@@ -9,6 +9,8 @@ public class Juego : MonoBehaviour
     public Material [] mate = new Material [16];
     int cont = 0;
     public bool rotar = false;
+    public GameObject bloque1;
+    public GameObject bloque2;
     
 
     public void OnMouseDown()
@@ -44,13 +46,41 @@ public class Juego : MonoBehaviour
            bloques[cont].GetComponent<Renderer>().material = mate[cont];
            cont += 1;
        }
+
+        cont = bloques.Count;
+        Debug.Log(cont);
     }
 
    
     void Update()
     {
-        
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, Mathf.Infinity))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("ledi raycast");
+                if (bloque1 == null)
+                {
+                    bloque1 = gameObject.GetComponent<Juego>().bloques.Equals(mat[].ToString);
+                    Debug.Log("entre aqui >:v");
+                }
+            }
+        }
+
     }
-   
+  /* public bool Compararbloque(GameObject bloque1 , GameObject bloque2 )
+    {
+        bool resultado;
+        if(bloque1.GetComponent<Juego>().bloques.ToString == bloque2.GetComponent<Juego>().bloques.ToString)
+        {
+            resultado = true;
+        }
+        else
+        {
+            resultado = false;
+        }
+    }*/
     
 }
---
+++ Assets/PruebaRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaRaycast : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, Mathf.Infinity))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("le di");
+                Debug.DrawRay(transform.position, ray.direction, Color.yellow);
+            }
+           
+        }
+       /* if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+
+            }
+        }*/
+    }
+}
