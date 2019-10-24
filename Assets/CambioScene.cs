a8790a0e51f3da3dc871fd30eb12264c3d9b7e04
Kazui97 <camyescobar12@gmail.com>
2019-10-24 17:08:49 -0500

    ya casi .. falta poco :dragon_face:

---
+++ Assets/CambioScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioScene : MonoBehaviour
+{
+    
+    public void CambioEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+}
